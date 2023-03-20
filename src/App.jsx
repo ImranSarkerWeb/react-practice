3c5403bf7e7ace9c22fda57ff598320aaba86827
imransarkerweb <imransarker.web@gmail.com>
2023-03-21 00:27:54 +0600

    useState() practice

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,27 @@ const friendList = [
 function App() {
   const friends = ["Mozammel", "Naser", "Sharup", "Sultana"]
 
+  const [count, setCount] = useState(0);
+  const countIncrease = () => setCount(count + 1);
+  const countDecrease = ()=> setCount(count-1);
+
+  const [name, setName] = useState('Imran');
+  const randomName = () => setName(friends[Math.floor(Math.random()* friends.length)])
+  
   return (
     
     <div className='app'>
     {
       friendList.map(friend => <Friend name={friend.name} phone={friend.phone}></Friend>)
     }
+
+    {/* use state  */}
+    <p>{count}</p>
+    <button onClick={countIncrease}>Increase</button>
+    <button onClick={countDecrease}>Decrease</button>
+
+    <p>{name}</p>
+    <button onClick={randomName}>Random Name</button>
     {/* <Friend name="Mozmmeml" phone="+0711499999" > </Friend> */}
     </div>
   )
