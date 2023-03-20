035510e910cf3c438e478bbff0b0568d431cfae1
imransarkerweb <imransarker.web@gmail.com>
2023-03-20 22:21:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Friend from './Friend'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <Friend name="Mozmmeml" phone="+0711499999" > </Friend>
+  )
+}
+
+export default App
---
+++ src/Friend.jsx
@@ -0,0 +1,10 @@
+function Friend({name, phone}){
+    return(
+        <div>
+            <h2>Name: {name}</h2>
+            <p>Phone: {phone} </p>
+        </div>
+    );
+}
+
+export default Friend;
