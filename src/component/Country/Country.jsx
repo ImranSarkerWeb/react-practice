d938e3923a73aaeacead05b3cb0468bde6a97599
imransarkerweb <imransarker.web@gmail.com>
2023-03-24 20:31:09 +0600

    updated

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./Country.css";
 const Country = (props) => {
   //   console.log(props);
+  const { name, flags } = props.country;
   return (
     <div className="country">
-      <h3>Country Name: {props.country.name.common}</h3>
-      <img src={props.country.flags.png} alt="" />
+      <h3>Country Name: {name.common}</h3>
+      <img src={flags.png} alt="" />
     </div>
   );
 };
